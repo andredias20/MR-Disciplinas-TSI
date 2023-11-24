f57de8bc31afe10cfd208a55b2ac72473ca9cd3d
andredias20 <andre.c.dias2000@gmail.com>
2023-11-23 21:58:39 -0300

    update-time

---
+++ TSI35D-PADROES/observer/src/main/java/com/github/andredias20/observer/fusca/motors/Motor1200.java
@@ -0,0 +1,58 @@
+package com.github.andredias20.observer.fusca.motors;
+
+import com.github.andredias20.observer.motor.Motor;
+import com.github.andredias20.observer.motor.MotorState;
+
+class Motor1200 implements Motor
+{
+
+	@Override
+	public MotorState getState()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isOn()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isOff()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void slowDown(float percent)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void speedUp(float percent)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ TSI35D-PADROES/observer/src/main/java/com/github/andredias20/observer/fusca/motors/MotorFactory.java
@@ -0,0 +1,13 @@
+package com.github.andredias20.observer.fusca.motors;
+
+public class MotorFactory {
+
+    public static enum MotorType {
+        FUSCA_1100,
+        FUSCA_1200,
+        FUSCA_1300,
+        FUSCA_1500,
+        FUSCA_1600
+
+    };
+}
